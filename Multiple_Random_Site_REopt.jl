ab53a2f23f6deab91840f2301001a9b021925f65
Bernal, Daniel <Daniel.Bernal@nrel.gov>
2024-07-02 20:42:00 -0600

    updated code and added xlsx file

--- Multiple_Random_Site_REopt.jl
+++ Multiple_Random_Site_REopt.jl
@@ -45,7 +45,7 @@ function generate_random_latitude()
     #add the minimum bound of lat to ensure within US region
     lat = rand() * (max_lat - min_lat) + min_lat
    
-    return lat, lon
+    return lat
 end
 
 # Create Random Generator function to produce random lat/long locations within the US
@@ -58,7 +58,7 @@ function generate_random_longitude()
     
     lon = rand() * (max_lon - min_lon) + min_lon
     
-    return lat, lon
+    return lon
 end
 
 # Create Random Generator function to produce random annual energy consumptions for electricity
@@ -87,8 +87,44 @@ function generate_random_demand_charge()
     #low bound and upper bound per kWh
     min_dem = 15.00
     max_dem = 250.00
-    elec_tariff = rand() * (max_dem - min_dem) + min_dem
+    elec_demand_charge = rand() * (max_dem - min_dem) + min_dem
     return elec_demand_charge
+end
+
+#random generator to create random cost per kW for PV 
+function generate_random_PV_cost()
+    #low bound and upper bound per kWh
+    min_cost = 1700
+    max_cost = 4200
+    PV_cost = rand() * (max_cost - min_cost) + min_cost
+    return PV_cost
+end
+
+#random generator to create random cost per kW for Wind
+function generate_random_Wind_cost()
+    #low bound and upper bound per kWh
+    min_cost = 2386
+    max_cost = 8000
+    Wind_cost = rand() * (max_cost - min_cost) + min_cost
+    return Wind_cost
+end
+
+#random generator to create random space (ft squared) constraints
+function generate_random_roofspace()
+    #low bound and upper bound per kWh
+    min_space = 1000
+    max_space = 1000000
+    roof_space = rand() * (max_space - min_space) + min_space
+    return roof_space
+end
+
+#random generator to create random land space in acres
+function generate_random_landspace()
+    #low bound and upper bound per kWh
+    min_space = 0
+    max_space = 10
+    land_space = rand() * (max_space - min_space) + min_space
+    return land_space
 end
 
 doe_reference_building_list = [ "FastFoodRest", "FullServiceRest", "Hospital", "LargeHotel",  "LargeOffice", "MediumOffice", "MidriseApartment", "Outpatient", "PrimarySchool",
@@ -101,13 +137,10 @@ NEM_list = [0, 25, 50, 100, 200, 250, 300, 400, 500, 750, 1000, 1500, 2000, 2500
 #PV location
 PV_location = ["both", "ground", "roof" ]
 
-# Generate and print a random location
-random_location = generate_random_location()
-println("This is a Random Location test run: Latitude: ", random_location[1], ", Longitude: ", random_location[2])
-
 # Set up general inputs for randomized locations
 data_file = "General_Inputs.json"
 input_data = JSON.parsefile("scenarios/$data_file")
+println("Successfuly parsed input data JSON file")
 
 # up to 1000 runs
 REopt_runs = fill(1, 1000)
@@ -124,7 +157,22 @@ for i in sites_iter
     input_data_site["ElectricTariff"]["blended_annual_demand_rate"] = generate_random_demand_charge()
     input_data_site["ElectricTariff"]["blended_annual_energy_rate"] = generate_random_electric_tariff()
     input_data_site["ElectricUtility"]["net_metering_limit_kw"] = rand(NEM_list)
+    input_data_site["ElectricTariff"]["wholesale_rate"] = rand() * input_data_site["ElectricTariff"]["blended_annual_energy_rate"]
     input_data_site["PV"]["location"] = rand(PV_location)
+
+    #if loop for space availability
+    if input_data_site["PV"]["location"] == "ground"
+        input_data_site["Site"]["land_acres"] = generate_random_landspace()
+    elseif input_data_site["PV"]["location"] == "roof"
+        input_data_site["Site"]["roof_squarefeet"] = generate_random_roofspace()
+    else
+        input_data_site["Site"]["roof_squarefeet"] = generate_random_roofspace()
+        input_data_site["Site"]["land_acres"] = generate_random_landspace()
+    end
+
+    input_data_site["Wind"]["installed_cost_per_kw"] = generate_random_Wind_cost()
+    input_data_site["PV"]["installed_cost_per_kw"] = generate_random_PV_cost()
+
     
     s = Scenario(input_data_site)
     inputs = REoptInputs(s)
@@ -146,5 +194,66 @@ for i in sites_iter
 
     results = run_reopt([m1,m2], inputs)
     append!(site_analysis, [(input_data_site, results)])
+    println("Completed Optimization Run #$i")
 end
 println("Completed Optimization")
+
+# Populate the DataFrame with the results produced and inputs
+df = DataFrame(
+    input_Latitude = [safe_get(site_analysis[i][2], ["Site", "latitude"]) for i in sites_iter],
+    input_Longitude = [safe_get(site_analysis[i][2], ["Site", "longitude"]) for i in sites_iter],
+    input_PV_location = [safe_get(site_analysis[i][2], ["PV", "location"])for i in sites_iter],
+    input_PV_installed_cost = [round(safe_get(site_analysis[i][2], ["PV", "installed_cost_per_kw"]), digits=2) for i in sites_iter],
+    input_Wind_size = [round(safe_get(site_analysis[i][2], ["Wind", "size_kw"]), digits=4) for i in sites_iter],
+    input_Wind_installed_cost = [round(safe_get(site_analysis[i][2], ["Wind", "installed_cost_per_kw"]), digits=2) for i in sites_iter],
+    input_Site_electric_load = [round(safe_get(site_analysis[i][2], ["ElectricLoad", "annual_kwh"]), digits=0) for i in sites_iter],
+    input_Site_building_type = [safe_get(site_analysis[i][2], ["ElectricLoad", "doe_reference_name"]) for i in sites_iter],
+    input_Site_roofspace = [round(safe_get(site_analysis[i][2], ["Site", "roof_squarefeet"]), digits=0) for i in sites_iter],
+    input_Site_landspace = [round(safe_get(site_analysis[i][2], ["Site", "land_acres"]), digits=0) for i in sites_iter],
+    input_Site_NEM_limit = [round(safe_get(site_analysis[i][2], ["ElectricUtility", "net_metering_limit_kw"]), digits=0) for i in sites_iter],
+    input_Site_net_billing_rate = [round(safe_get(site_analysis[i][2], ["ElectricTariff", "wholesale_rate"]), digits=2) for i in sites_iter],
+    input_Site_electricity_cost_per_kwh = [round(safe_get(site_analysis[i][2], ["ElectricTariff", "blended_annual_energy_rate"]), digits=2) for i in sites_iter],
+    input_Site_demand_charge_cost_per_kw = [round(safe_get(site_analysis[i][2], ["ElectricTariff", "blended_annual_demand_rate"]), digits=2) for i in sites_iter],
+    output_PV_size = [round(safe_get(site_analysis[i][2], ["PV", "size_kw"]), digits=0) for i in sites_iter],
+    output_PV_energy_lcoe = [round(safe_get(site_analysis[i][2], ["PV", "lcoe_per_kwh"]), digits=0) for i in sites_iter],
+    output_PV_energy_exported = [round(safe_get(site_analysis[i][2], ["PV", "annual_energy_exported_kwh"]), digits=0) for i in sites_iter],
+    output_PV_energy_curtailed = [sum(safe_get(site_analysis[i][2], ["PV", "electric_curtailed_series_kw"], 0)) for i in sites_iter],
+    output_Wind_size = [round(safe_get(site_analysis[i][2], ["Wind", "size_kw"]), digits=0) for i in sites_iter],
+    output_Wind_energy_lcoe = [round(safe_get(site_analysis[i][2], ["Wind", "lcoe_per_kwh"]), digits=0) for i in sites_iter],
+    output_Wind_energy_exported = [round(safe_get(site_analysis[i][2], ["Wind", "annual_energy_exported_kwh"]), digits=0) for i in sites_iter],
+    output_Wind_energy_curtailed = [sum(safe_get(site_analysis[i][2], ["Wind", "electric_curtailed_series_kw"], 0)) for i in sites_iter],
+    output_Grid_Electricity_Supplied_kWh_annual = [round(safe_get(site_analysis[i][2], ["ElectricUtility", "annual_energy_supplied_kwh"]), digits=0) for i in sites_iter],
+    output_npv = [round(safe_get(site_analysis[i][2], ["Financial", "npv"]), digits=2) for i in sites_iter],
+    output_lcc = [round(safe_get(site_analysis[i][2], ["Financial", "lcc"]), digits=2) for i in sites_iter]
+    )
+println(df)
+
+# Define path to xlsx file
+file_storage_location = "C:/Users/dbernal/Documents/GitHub/Public_REopt_analysis/results/REopt_data.xlsx"
+
+# Check if the Excel file already exists
+if isfile(file_storage_location)
+    # Open the Excel file in read-write mode
+    XLSX.openxlsx(file_storage_location, mode="rw") do xf
+        counter = 0
+        while true
+            sheet_name = "Results" * string(counter)
+            try
+                sheet = xf[sheet_name]
+                counter += 1
+            catch
+                break
+            end
+        end
+        sheet_name = "Results" * string(counter)
+        # Add new sheet
+        XLSX.addsheet!(xf, sheet_name)
+        # Write DataFrame to the new sheet
+        XLSX.writetable!(xf[sheet_name], df)
+    end
+else
+    # Write DataFrame to a new Excel file
+    XLSX.writetable!(file_storage_location, df)
+end
+
+println("Successful write into XLSX file: $file_storage_location")
